50ec0042f041766a933e76801ac1e013b063ae9b
Steven Combs <scombs@pillartechnology.com>
2019-03-20 14:48:52 -0500

    consolidated for loops
    Signed-off-by: Veronica Stafford <Veronica.Stafford@LSIowa.org>

--- src/main/java/FooBarQix.java
+++ src/main/java/FooBarQix.java
@@ -2,31 +2,28 @@ public class FooBarQix {
     public String playGame(int number) {
         String playerEntered = "";
 
-        if(number % 3 == 0){
+        if (number % 3 == 0) {
             playerEntered = "foo";
         }
-        if(number % 5 == 0) {
+        if (number % 5 == 0) {
             playerEntered = "bar";
         }
 
-        if(number % 7 == 0){
+        if (number % 7 == 0) {
             playerEntered = "qix";
         }
 
         String stringNum = String.valueOf(number);
-        for(int i =0; i < stringNum.length(); i++) {
-            if('3' == stringNum.charAt(i)) {
-                playerEntered+="foo";
+        for (int i = 0; i < stringNum.length(); i++) {
+            if ('3' == stringNum.charAt(i)) {
+                playerEntered += "foo";
+            }
+            if ('5' == stringNum.charAt(i)) {
+                playerEntered += "bar";
             }
         }
 
-        for(int i =0; i < stringNum.length(); i++) {
-            if('5' == stringNum.charAt(i)) {
-                playerEntered+="bar";
-            }
-        }
-
-        if(playerEntered.equals("")) {
+        if (playerEntered.equals("")) {
             playerEntered = stringNum;
         }
 
