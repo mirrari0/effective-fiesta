d6bb47460cdf0366fe5cc8b82bd7f8760cad6208
Steven Combs <scombs@pillartechnology.com>
2019-03-20 14:31:25 -0500

    two test scenario with foo test
    Signed-off-by: Veronica Stafford <Veronica.Stafford@LSIowa.org>

--- src/main/java/FooBarQix.java
+++ src/main/java/FooBarQix.java
@@ -2,6 +2,10 @@ public class FooBarQix {
     public String playGame(int i) {
         String playerEntered = String.valueOf(i);
 
+        if(i % 3 == 0){
+            playerEntered = "foo";
+        }
+
         return playerEntered;
     }
 }
