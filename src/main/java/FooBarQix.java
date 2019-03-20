d7ba7b1181eef9bf6b580e6c6956e889d1e08177
Steven Combs <scombs@pillartechnology.com>
2019-03-20 14:29:46 -0500

    initial plus test one
    Signed-off-by: Veronica Stafford <Veronica.Stafford@LSIowa.org>

---
+++ src/main/java/FooBarQix.java
@@ -0,0 +1,7 @@
+public class FooBarQix {
+    public String playGame(int i) {
+        String playerEntered = String.valueOf(i);
+
+        return playerEntered;
+    }
+}
---
+++ src/test/java/FooBarQixTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FooBarQixTest {
+
+    @Test
+    public void given_three_returns_foo(){
+        assertEquals( "foo", new FooBarQix().playGame(3));
+    }
+
+    @Test
+    public void give_a_one_should_return_one() {
+        assertEquals("1", new FooBarQix().playGame(1));
+    }
+}
